62f64d665358266aa50cb551a90731509c8e21f8
dsindjelic <dsindjelic@live.com>
2020-05-21 16:39:02 +0200

    obrisani fajlovi

--- Scheduling/src/routes.js
+++ Scheduling/src/routes.js
@@ -1,15 +1,10 @@
-import showBlogs from './components/showBlogs.vue'
-import addBlog  from './components/addBlog.vue'
-import singleBlog from './components/singleBlog.vue'
 import addUser from './components/addUser.vue'
 import addBusiness from './components/addBusiness.vue'
 import schedulingClient from  './components/schedulingClient.vue'
 
 export default[
-    {path:'/', component:showBlogs},
-    {path:'/add', component:addBlog},
+    {path:'/', component:schedulingClient},
     {path:'/adduser', component:addUser},
     {path:'/addbusiness', component:addBusiness},
-    {path:'/blog/:id', component:singleBlog},
     {path:'/schedulingclient', component:schedulingClient}
 ]
