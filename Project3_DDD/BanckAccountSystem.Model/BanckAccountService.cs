332862221d9afd49960b04b037d86da5393f8378
dsindjelic <dsindjelic@live.com>
2020-08-23 23:29:29 +0200

    added domain service (e.g.  anckAccountService class)

---
+++ Project3_DDD/BanckAccountSystem.Model/BanckAccountService.cs
@@ -0,0 +1,32 @@
+﻿using BanckAccountSystem.Model.Contract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BanckAccountSystem.Model
+{
+   public   class BanckAccountService
+    {
+        private IBanckAccountRepository _banckAccountRepository;
+        public BanckAccountService(IBanckAccountRepository bankAccountRepository)
+        {
+            _banckAccountRepository = bankAccountRepository;
+        }
+        public void Transfer(Guid accountNoTo, Guid accountNoFrom, decimal amount)
+        {
+            BanckAccount banckAccountTo = _banckAccountRepository.FindBy(accountNoTo);
+            BanckAccount banckAccountFrom = _banckAccountRepository.FindBy(accountNoFrom);
+            if (banckAccountFrom.CanWithdraw(amount))
+            {
+                banckAccountTo.Deposit(amount, "From Acc " + banckAccountFrom.CustomerRef + " ");
+                banckAccountFrom.Withdraw(amount, "Transfer To Acc " + banckAccountTo.CustomerRef + " ");
+                _banckAccountRepository.Save(banckAccountTo);
+                _banckAccountRepository.Save(banckAccountFrom);
+            }
+            else
+            {
+                throw new InsufficientFundsException();
+            }
+        }
+    }
+}
---
+++ Project3_DDD/BanckAccountSystem.Model/Contract/IBanckAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BanckAccountSystem.Model.Contract
+{
+   public interface IBanckAccountRepository
+    {
+        void Add(BanckAccount banckAccount);
+        void Save(BanckAccount banckAccount);
+        IEnumerable<BanckAccount> FindAll();
+        BanckAccount FindBy(Guid AccountId);
+    }
+}
